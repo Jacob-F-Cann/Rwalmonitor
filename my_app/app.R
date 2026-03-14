688534466a669c9c991aa64c29604290e9c5446e
Jacob Cann <jfc25@student.ubc.ca>
2026-03-14 14:03:58 -0700

    app.R progress

--- my_app/app.R
+++ my_app/app.R
@@ -3,45 +3,60 @@ library(bslib)
 
 # Skeleton from: https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/
 
+# Read Data
+df <- read.csv('../data/raw/walmart_sales_data.csv')
+# Clean up data noise per day, assign date object column
+df <- df |> mutate(Date = as.Date(Date)) |> 
+  group_by(Date) |>
+  summarise(across(where(is.numeric), sum))
+
+# Defaults:
+metrics_default <- c('Total', 'cogs')
+time_default <- c('2019-01-01', '2019-02-01')
+
 # Define UI for app that draws a histogram ----
 ui <- page_sidebar(
   # App title ----
-  title = "Hello Shiny!",
-  # Sidebar panel for inputs ----
+  title = "Rwalmonitor",
+  # Sidebar panel for date inputs ----
   sidebar = sidebar(
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
-    )
+  sliderInput(
+    inputId = "date_range",
+    label = "Selected Date Range",
+    min = as.Date("2019-01-01"),
+    max = as.Date("2019-03-30"),
+    value = c(as.Date(time_default)[1], as.Date(time_default[2])),
+    timeFormat = "%Y-%m-%d"
+  )
   ),
+  
   # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+  plotOutput(outputId = "LinePlot")
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
+  
+  filtered_data <- reactive(
+    {
+    df |> filter(Date >= input$date_range[1], Date <= input$date_range[2])
+    }
+  )
+  
+  return(filtered_data)
+  
+  
+  output$LinePlot <- renderPlot({
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#007bc2", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
+    filtered_data() |> pivot_longer(cols = all_of(metrics_default),
+                                names_to = "variable", values_to = "value") |>
+      filter(Date >= time_default[1], Date <= time_default[2]) |> 
+      ggplot(aes(x = Date, y=value, color=variable))+
+      geom_line()+
+      labs(y = paste(metrics_default, collapse = ", "),
+           title = 'Selected Metrics Over Time')
+  
   })
   
 }
